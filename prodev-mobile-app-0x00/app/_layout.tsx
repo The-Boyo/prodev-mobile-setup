e50a6e36d667820965846098877589817024251c
Eliud Metto <eliudmett093@gmail.com>
2025-08-29 09:04:31 +0300

    Added text to the the home screen

--- prodev-mobile-app-0x00/app/_layout.tsx
+++ prodev-mobile-app-0x00/app/_layout.tsx
@@ -2,6 +2,6 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return <Stack>
-    <Stack.Screen name='index.tsx' />
+    <Stack.Screen name='index' />
   </Stack>;
 }
